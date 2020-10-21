9d4472b693300be4bd7767278b2200e10915101c
JoelVG <jvg95@hotmail.es>
2020-10-21 23:51:41 +0200

    Práctica de Picasso para la asignatura de móviles.

---
+++ Eval1/MusicPlayer_JVG/app/src/main/java/com/example/musicplayer_jvg/resources/UrlValues.java
@@ -0,0 +1,11 @@
+package com.example.musicplayer_jvg.resources;
+
+public class UrlValues {
+    public final static String GARY_MOORE="https://www.multiocio.com/8972-large_default/gary-moore-blues-alive-vinilo.jpg";
+    public final static String PINK_FLOYD="https://hipertextual.com/files/2012/01/Pink1.jpg";
+    public final static String METALLICA="https://images-na.ssl-images-amazon.com/images/I/51O7k2hqS1L._SY355_.jpg";
+    public final static String GARY_MOORE_SONG="https://www.youtube.com/watch?v=ewUgJQHCh8A";
+    public final static String PINK_FLOYD_SONG="https://www.youtube.com/watch?v=5IpYOF4Hi6Q";
+    public final static String METALLICA_SONG="https://www.youtube.com/watch?v=mmIXwYkDs9k";
+
+}
---
+++ Eval1/MusicPlayer_JVG/app/src/main/java/com/example/musicplayer_jvg/resources/Values.java
@@ -0,0 +1,17 @@
+package com.example.musicplayer_jvg.resources;
+
+public class Values {
+    public final static int CONT_INICIO=0;
+    public final static int EMPTY=0;
+    public final static int TIME_BETWEEN_IMG=5000;
+    public final static int TIME_DELAY_IMG=1;
+    public final static int CONT_LIMIT=2;
+    public final static int NUM_OF_IMGS=3;
+    public final static int IMG_CODE=0;
+    public final static int SONG_CODE=1;
+    public final static int REQUEST_CODE=1;
+    public final static int FIRST_ON_ARRAY=0;
+    public final static int SECOND_ON_ARRAY=1;
+    public final static int THIRD_ON_ARRAY=2;
+    public final static int QUALITY=100;
+}
